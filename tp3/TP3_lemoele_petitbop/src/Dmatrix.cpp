2689da20c8b255bf0233a30178e3fd0d21184f5b
Emilie Le Moël <elemoel@eduroam-033248.grenet.fr>
2015-03-02 15:31:21 +0100

    Constructeurs Matrix + début destructeur

--- tp3/TP3_lemoele_petitbop/src/Dmatrix.cpp
+++ tp3/TP3_lemoele_petitbop/src/Dmatrix.cpp
@@ -6,3 +6,28 @@
 //!
 
 #include "Dmatrix.h"
+
+//=============================================================================
+//								CONSTRUCTORS
+//=============================================================================
+Dmatrix::Dmatrix() : Darray() {
+	m = 0;
+	n = 0;
+}
+
+Dmatrix::Dmatrix(int lines, int columns, double val) : Darray(lines*columns, val) {
+	m = lines;
+	n = columns;
+}
+
+Dmatrix::Dmatrix(Dmatrix const& v) : Darray(v) {
+	m = v.m;
+	n = v.n;
+}
+
+//=============================================================================
+//								DESTRUCTOR
+//=============================================================================
+Dmatrix::~Dmatrix() {
+	
+}
--- tp3/TP3_lemoele_petitbop/src/Dmatrix.h
+++ tp3/TP3_lemoele_petitbop/src/Dmatrix.h
@@ -17,9 +17,35 @@
 //!
 class Dmatrix : public Darray {
 
-	private:
+	private :
 		int m; 		/* nombre de lignes */
 		int n; 		/* nombre de colonnes */
+
+	public :
+
+		//=============================================================================
+    	//								CONSTRUCTORS
+		//=============================================================================
+        //! @description 	Default constructor.
+        Dmatrix();
+
+        //! @description 	Constructor.
+        //! @param lines 	Initial number of lines of the matrix.
+        //! @param columns 	Initial number of columns of the matrix.
+        //! @param val		Initial value of all the components of the matrix.
+        //!					Optional parameter.
+        Dmatrix(int lines, int columns, double val = 0);
+
+        //! @description 	Copy constructor.
+        //! @param v 		Dmatrix instance from which to create a new one.
+        Dmatrix(Dmatrix const& v);
+
+		//=============================================================================
+    	//								DESTRUCTOR
+		//=============================================================================
+        //! @descritpion	Destructor
+        ~Dmatrix();
+
 
 };
 
